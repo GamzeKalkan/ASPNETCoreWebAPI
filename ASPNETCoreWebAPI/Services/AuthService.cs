4bf7e6d09d639cb2075296e850557e9a72cb9025
GamzeKalkan <gmz_bsrr@hotmail.com>
2023-11-24 13:45:11 +0300

    token-olusturabilmek-için-AuthService-classına-eklemeler-yapıldı

--- ASPNETCoreWebAPI/Services/AuthService.cs
+++ ASPNETCoreWebAPI/Services/AuthService.cs
@@ -1,13 +1,42 @@
-﻿using ASPNETCoreWebAPI.Models;
-
+﻿
+using ASPNETCoreWebAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 namespace ASPNETCoreWebAPI.Services
 {
     public class AuthService
     {
+        private readonly string _secretKey = "your_secret_key";
+        private readonly int _tokenExpirationInMinutes = 30;
         public bool ValidateCredentials(Users user)
         {
 
             return user.Mail == "example" && user.Password == "password";
+        }
+
+        public string GenerateJwtToken(Users user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Mail),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: "your_issuer",
+                audience: "your_audience",
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_tokenExpirationInMinutes),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
