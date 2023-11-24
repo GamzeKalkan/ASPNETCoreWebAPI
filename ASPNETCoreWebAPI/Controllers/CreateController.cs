786619c0922882d40d2feab9723023d183cf25fd
GamzeKalkan <gmz_bsrr@hotmail.com>
2023-11-24 13:36:29 +0300

    User-Create-methodu-için-Create-Controller-olusturuldu

---
+++ ASPNETCoreWebAPI/Controllers/CreateController.cs
@@ -0,0 +1,33 @@
+﻿using ASPNETCoreWebAPI.Models;
+using ASPNETCoreWebAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreateController : ControllerBase
+    {
+        private readonly AddService _Service;
+
+        public CreateController(AddService Service)
+        {
+            _Service = Service;
+        }
+
+        [HttpPost("add-users")]
+        public IActionResult AddUser([FromBody] Users users)
+        {
+            _Service.AddUser(users);
+            return Ok("User added successfully.");
+        }
+
+        [HttpPost("add-address")]
+        public IActionResult AddAddress([FromBody] Addresses address)
+        {
+            _Service.AddAddress(address);
+            return Ok("Address added successfully.");
+        }
+    }
+}
