095f32759b94fedcbf251e5152829f75bd21031d
GamzeKalkan <gmz_bsrr@hotmail.com>
2023-11-24 13:47:39 +0300

    Login-methodu-için-AuthController-oluşturuldu-,-sorunsuz-giriş-olduğunda-token-oluşması-sağlandı

---
+++ ASPNETCoreWebAPI/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using ASPNETCoreWebAPI.Models;
+using ASPNETCoreWebAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] Users user)
+        {
+
+            if (_authService.ValidateCredentials(user))
+            {
+
+
+                var token = _authService.GenerateJwtToken(user);
+
+
+                return Ok(new { Token = token });
+            }
+
+
+            return Unauthorized("Kullanıcı adı veya şifre hatalı.");
+        }
+    }
+}
