4f436f0451cd8f5ba54b3da37673cea2a7025431
GamzeKalkan <gmz_bsrr@hotmail.com>
2023-11-24 13:23:29 +0300

    InMemory-Database-bağlantısı-için-DataContext-klasörü-oluşturularak-içerinde-Context-classında-DBContext-bağlantısı-yapıldı

---
+++ ASPNETCoreWebAPI/DataContext/Context.cs
@@ -0,0 +1,25 @@
+﻿using ASPNETCoreWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETCoreWebAPI.DataContext
+{
+    public class Context:DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Addresses> Addresses { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure the one-to-many relationship
+            modelBuilder.Entity<Users>()
+                .HasMany(p => p.Address)
+                .WithOne(a => a.Users)
+                .HasForeignKey(a => a.UserId);
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("apiDatabase");
+        }
+    }
+}
---
+++ ASPNETCoreWebAPI/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETCoreWebAPI.Models
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+
+        public List<Addresses> Address { get; set; }
+    }
+}
