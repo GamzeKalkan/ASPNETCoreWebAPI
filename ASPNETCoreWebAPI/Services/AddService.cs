3506543760cfd0d09c84f0f39eb2b9541118cd9f
GamzeKalkan <gmz_bsrr@hotmail.com>
2023-11-24 13:34:10 +0300

    User-ve-address-ekleme-işlemleri-için-Services-klasörü-açılarak-içerisinde-AddService-class-ı-oluşturulup-kayıt-işlemlerinin-burada-oluşturuldu

---
+++ ASPNETCoreWebAPI/Services/AddService.cs
@@ -0,0 +1,27 @@
+﻿using ASPNETCoreWebAPI.DataContext;
+using ASPNETCoreWebAPI.Models;
+
+namespace ASPNETCoreWebAPI.Services
+{
+    public class AddService
+    {
+        private readonly Context _context;
+
+        public AddService(Context context)
+        {
+            _context = context;
+        }
+
+        public void AddUser(Users users)
+        {
+            _context.Users.Add(users);
+            _context.SaveChanges();
+        }
+
+        public void AddAddress(Addresses address)
+        {
+            _context.Addresses.Add(address);
+            _context.SaveChanges();
+        }
+    }
+}
