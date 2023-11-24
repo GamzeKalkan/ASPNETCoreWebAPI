1668c5c3251538bc1c4aa937b9bd845a9c2636e2
GamzeKalkan <gmz_bsrr@hotmail.com>
2023-11-24 13:38:50 +0300

    Login-için-Service-klasörünün-içine-AuthService-classı-oluşturularak,-mail--ve-şifre-kontrolü-oluşturuldu

---
+++ ASPNETCoreWebAPI/Services/AuthService.cs
@@ -0,0 +1,13 @@
+﻿using ASPNETCoreWebAPI.Models;
+
+namespace ASPNETCoreWebAPI.Services
+{
+    public class AuthService
+    {
+        public bool ValidateCredentials(Users user)
+        {
+
+            return user.Mail == "example" && user.Password == "password";
+        }
+    }
+}
