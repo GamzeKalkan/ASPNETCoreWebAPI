410ba13e7e1339654e0cfdafb7adb20cf6764d0e
GamzeKalkan <gmz_bsrr@hotmail.com>
2023-11-24 13:23:29 +0300

    Users-ve-Addresses-tabloları-Models-klasörü-acilarak-icerisinde-olusturuldu

---
+++ ASPNETCoreWebAPI/Models/Addresses.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETCoreWebAPI.Models
+{
+    public class Addresses
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int UserId { get; set; }
+        public Users Users { get; set; }
+    }
+}
